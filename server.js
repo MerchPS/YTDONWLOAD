61a4aa53ea898e9d2792be70063d11a873f51332
MerchPS <merchpsbackup@gmail.com>
2025-02-01 19:10:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
 
 app.post('/api/info', async (req, res) => {
     const { url } = req.body;
@@ -18,6 +20,11 @@ app.post('/api/info', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
